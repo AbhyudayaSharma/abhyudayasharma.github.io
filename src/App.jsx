51456d8eb4e259bf93222cadb3b40b321b8ebe54
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2020-01-18 15:33:07 +0530

    Fix homepage flexbox css

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,28 @@ function App() {
   const gitInfo = GitInfo();
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <h1 className='unselectable'>
           Abhyudaya Sharma
         </h1>
-      </header>
+      </div>
       <div className="App-body">
         <BigButton text="GitHub" url="https://github.com/AbhyudayaSharma"/>
         <BigButton text="LinkedIn" url="https://www.linkedin.com/in/abhyudaya-sharma/"/>
       </div>
       <div className='App-footer unselectable'>
-        This page was built and deployed from the commit&nbsp;
-        <a href={`https://github.com/AbhyudayaSharma/abhyudayasharma.github.io/commit/${gitInfo.commit.hash}`}
-          className='App-link selectable'>
-          <code>{gitInfo.commit.shortHash}</code>
-        </a>
-        <br/>
-        Fork this repository on&nbsp;
-        <a href='https://github.com/AbhyudayaSharma/abhyudayasharma.github.io' className='selectable App-link'>
-          GitHub
-        </a>
+        <p>
+          This page was built and deployed from the commit&nbsp;
+          <a href={`https://github.com/AbhyudayaSharma/abhyudayasharma.github.io/commit/${gitInfo.commit.hash}`}
+            className='App-link selectable'>
+            <code>{gitInfo.commit.shortHash}</code>
+          </a>
+          <br/>
+          Fork this repository on&nbsp;
+          <a href='https://github.com/AbhyudayaSharma/abhyudayasharma.github.io' className='selectable App-link'>
+            GitHub
+          </a>
+        </p>
       </div>
     </div>
   );
