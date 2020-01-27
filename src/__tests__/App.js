018f3705e15ed5b09dbaa421315079180b82a12c
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2020-01-27 18:39:45 +0530

    Add tests for new urls

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -4,6 +4,10 @@ import App from '../App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App/>, div);
   ReactDOM.unmountComponentAtNode(div);
+  const urls = ['/', '/blog', '/about'];
+  for (let i = 0; i < urls.length; i++) {
+    window.location = `/#${urls[i]}`;
+  }
 });
