3f1363d0598306e4aecf264f184704514662a297
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2020-08-28 12:14:42 +0530

    Revert "Add support for rendering HTML at build time (#219)"
    This reverts commit c3ae77c67524c314a3cffd2698116385b2aace12.

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import { render, hydrate } from 'react-dom';
+import ReactDOM from 'react-dom';
 import 'whatwg-fetch';
 
 import './scss/index.scss';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-if (!rootElement || rootElement.tagName !== 'DIV') {
-  throw Error('Document should contain a div element with id=`root`.');
-}
-
-if (rootElement.hasChildNodes()) {
-  hydrate(<App/>, rootElement);
-} else {
-  render(<App/>, rootElement);
-}
+ReactDOM.render(<App />, document.getElementById('root'));
