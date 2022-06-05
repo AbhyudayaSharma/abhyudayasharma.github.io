aca43ac9fc7a0a51a8b139571371fa71af9d9edf
Abhyudaya Sharma <11471599+AbhyudayaSharma@users.noreply.github.com>
2022-06-05 17:57:51 +0530

    Add license information to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,14 +11,12 @@ export const Footer: React.FC = () => {
   return (
     <footer className={container}>
       <p>
+        Text under <a href='https://creativecommons.org/licenses/by-nc-nd/4.0/' className={link}>
+        CC-BY-NC-ND-4.0</a> • Source on <a href={repository.url} className={link}>GitHub</a>
+        <br/>
         This page was built and deployed from the commit&nbsp;
         <a href={`${repository.url}/commit/${gitInfo.commit.hash}`} className={link}>
           <code>{gitInfo.commit.shortHash}</code>
-        </a>
-        <br />
-        Fork this repository on&nbsp;
-        <a href={repository.url} className={link}>
-          GitHub
         </a>
       </p>
     </footer>
